40733dfe6395d086d4abe4602bdf5fef87bf81de
jesusepp <jesusepereira@gmail.com>
2020-06-23 22:23:42 -0400

    Changes for the animations and design for the "Courses" section of the WebPage

--- main/courses/courses.js
+++ main/courses/courses.js
@@ -1,8 +1,11 @@
 function selectMenu(n){
     let j = document.getElementById('' + n);
+    let z = j.previousElementSibling;
     if (j.style.maxHeight){
         j.style.maxHeight = null;
+        z.style.position = 'relative';
     } else {
         j.style.maxHeight = j.scrollHeight + "px";
+        z.style.position = 'sticky';
     }
 }
