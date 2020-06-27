5d5d9e841d0ff29f340f57ba55c8daa89b0d34d5
jesusepp <jesusepereira@gmail.com>
2020-06-27 12:05:35 -0400

    Changes to courses and certs

--- main/courses/courses.js
+++ main/courses/courses.js
@@ -1,10 +1,13 @@
 function selectMenu(n){
     let j = document.getElementById('' + n);
     let z = j.previousElementSibling;
+    let arrow = z.childNodes[3].childNodes[0].childNodes[0]
     if (j.style.maxHeight){
+        arrow.style.transform = 'rotate(-90deg)';
         j.style.maxHeight = null;
         z.style.position = 'relative';
     } else {
+        arrow.style.transform = 'rotate(0deg)';
         j.style.maxHeight = j.scrollHeight + "px";
         z.style.position = 'sticky';
     }
