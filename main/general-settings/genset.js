9ed5e80638bc403f5311b57d1d701840e9d4e1b5
jesusepp <jesusepereira@gmail.com>
2020-07-02 15:25:04 -0400

    Correccion

--- main/general-settings/genset.js
+++ main/general-settings/genset.js
@@ -1,4 +1,4 @@
-function insertHTML(){
+function insertHTML(type = 0){
     // Insert the header
     let header = document.getElementsByTagName('header');
     let x = "<div id='navigator' onclick=\"selectHeader('hiden-menu')\">";
@@ -19,19 +19,29 @@ function insertHTML(){
     x += "</div>";
     x += "</div>";
     header[0].innerHTML = x;
-    // Insert the footer
-    let footer = document.getElementsByTagName('footer');
-    let y = "<div id='cont-inf'>";
-    y += "<h3>Contact Information</h3>";
-    y += "<p class='redes'><span class='icon'><i class='fa fa-github-alt'></i></span><a href='https://github.com/jesusepp'>GitHub</a></p>";
-    y += "<p class='redes'><span class='icon'><i class='fa fa-linkedin'></i></span><a href='https://www.linkedin.com/in/jeppires'>LinkedIn</a></p>";
-    y += "<p class='redes'><span class='icon'><i class='fa fa-at'></i></span>jpereira.fsaeusb@gmail.com</p>";
-    y += "</div>";
-    y += "<div id='mark'>";
-    y += "This web aplication was developed by José Pasos (<!-- Insertar su web page-->) and Jesús Pereira ";
-    y += "(<!-- Insertar su web page-->), Mechanical Engineering Students at 'Universidad Simón Bolívar'";
-    y += "</div>";
-    footer[0].innerHTML = y;
+    if (type == 1){
+        // Insert the footer
+        let footer = document.getElementsByTagName('footer');
+        let y = "<div id='mark'>";
+        y += "This web aplication was developed by José Pasos (<!-- Insertar su web page-->) and Jesús Pereira ";
+        y += "(<!-- Insertar su web page-->), Mechanical Engineering Students at 'Universidad Simón Bolívar'";
+        y += "</div>";
+        footer[0].innerHTML = y;
+    } else {
+        // Insert the footer
+        let footer = document.getElementsByTagName('footer');
+        let y = "<div id='cont-inf'>";
+        y += "<h3>Contact Information</h3>";
+        y += "<p class='redes'><span class='icon'><i class='fa fa-github-alt'></i></span><a href='https://github.com/jesusepp'>GitHub</a></p>";
+        y += "<p class='redes'><span class='icon'><i class='fa fa-linkedin'></i></span><a href='https://www.linkedin.com/in/jeppires'>LinkedIn</a></p>";
+        y += "<p class='redes'><span class='icon'><i class='fa fa-at'></i></span>jpereira.fsaeusb@gmail.com</p>";
+        y += "</div>";
+        y += "<div id='mark'>";
+        y += "This web aplication was developed by José Pasos (<!-- Insertar su web page-->) and Jesús Pereira ";
+        y += "(<!-- Insertar su web page-->), Mechanical Engineering Students at 'Universidad Simón Bolívar'";
+        y += "</div>";
+        footer[0].innerHTML = y;
+    }
 }
 
 function selectHeader(n){
