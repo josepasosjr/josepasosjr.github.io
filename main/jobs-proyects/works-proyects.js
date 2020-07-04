c571a8ae0269d05cca97ec22eeb87fd152d1fe75
jose pasos <josepasosjr@gmail.com>
2020-07-04 11:58:17 -0400

    jobs

--- main/jobs-proyects/works-proyects.js
+++ main/jobs-proyects/works-proyects.js
@@ -25,4 +25,12 @@ function lineDetection(){
     } 
 }
 
+function selectMenu(n){
+    let j = document.getElementById('' + n);
+    if (j.style.maxHeight){
+        j.style.maxHeight = null;
+    } else {
+        j.style.maxHeight = j.scrollHeight + "px";
+    }
+}
 
