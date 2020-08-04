6d9ff38248f5118d134237d653faa46626d07e11
jose pasos <josepasosjr@gmail.com>
2020-08-04 18:05:19 -0400

    contact done

--- main/general-settings/genset.js
+++ main/general-settings/genset.js
@@ -5,8 +5,8 @@ function insertHTML(type = 0){
     x += "<div class='downArrow medhf minhf'><span class='icon'><i class='fa fa-angle-down'></i></span></div>";
     x += "<div class='menus maxhf'><a href='../curriculum/curriculum.html'>CV</a></div>";
     x += "<div class='menus maxhf'><a href='../about-us/about-us.html'>About Me</a></div>";
-    x += "<div class='menus maxhf'><a href='../courses/courses.html'>Courses and Certifications</a></div>";
-    x += "<div class='menus maxhf'><a href='../jobs-proyects/jobs-proyects.html'>Jobs and Projects</a></div>";
+    x += "<div class='menus maxhf'><a href='../courses/courses.html'>Courses & Certifications</a></div>";
+    x += "<div class='menus maxhf'><a href='../jobs-proyects/jobs-proyects.html'>Jobs & Projects</a></div>";
     x += "<div class='menus maxhf'><a href='../contact-info/contact-info.html'>Contact Information</a></div>";
     x += "</div>";
     x += "<div class='medhf minhf' id='hiden-menu'>";
@@ -23,8 +23,8 @@ function insertHTML(type = 0){
         // Insert the footer
         let footer = document.getElementsByTagName('footer');
         let y = "<div id='mark'>";
-        y += "This web aplication was developed by José Pasos (<!-- Insertar su web page-->) and Jesús Pereira ";
-        y += "(<!-- Insertar su web page-->), Mechanical Engineering Students at 'Universidad Simón Bolívar'";
+        y += "This web aplication was developed by <a href='https://josepasosjr.github.io'>José Pasos</a> and <a href='https://jesusepp.github.io'>Jesús Pereira</a> ";
+        y += ", Mechanical Engineering Students at Universidad Simón Bolívar ";
         y += "</div>";
         footer[0].innerHTML = y;
     } else {
